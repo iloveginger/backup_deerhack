debdee23a5bc9dd03f3b421d2a423da08f6b365d
chrisdvkta <chrisdevkota77@gmail.com>
2024-02-10 00:13:41 +0545

    fix:build

--- frontend/src/app/components/Judges/data.ts
+++ frontend/src/app/components/Judges/data.ts
@@ -1,5 +1,5 @@
 export default async function FetchData() {
-  const response = await fetch("http://localhost:1337/api/judges");
+  const response = await fetch(process.env.link + "judges");
   const data = await response.json();
   return data;
 }
--- frontend/src/app/components/Mentors/Mentors.tsx
+++ frontend/src/app/components/Mentors/Mentors.tsx
@@ -1,10 +1,7 @@
 // import FetchData from "./data";
 import { cabinetBold, satoshiRegular, cabinetRegular } from "@/fonts";
-import MentorCard from "./MentorCardCard";
 
 const Judges = async () => {
-  const data = await FetchData();
-  console.log();
   return (
     <div key="container" className="w-[85%] m-auto py-10">
       <div key="judgeContainer" className="flex items-center flex-col my-20">
@@ -17,11 +14,7 @@ const Judges = async () => {
           key="JudgeCards"
         className="flex flex-wrap lg:w-full xl:w-[70%] mt-2 gap-16 justify-center"
         >
-          <JudgeCard name ="krish" position="dev" />
-          <JudgeCard name ="krish" position="dev"/>
-          <JudgeCard name ="krish" position="dev"/>
-          <JudgeCard name ="krish" position="dev"/>
-          <JudgeCard name ="krish" position="dev"/>
+          
         </div>
       </div>
     </div> 
