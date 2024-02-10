5ad269fc2b5a88ce683e57efd424996e2545c8f0
chrisdvkta <chrisdevkota77@gmail.com>
2024-02-11 00:11:50 +0545

    feat:Judges Integration with dev

--- frontend/src/app/components/Judges/Judges.tsx
+++ frontend/src/app/components/Judges/Judges.tsx
@@ -9,8 +9,6 @@ const Judges = async () => {
   const judges = response.data;
   let judges_real: Judge[] = [];
   for (let judge of judges) {
-    console.log(judge.attributes.image.data.attributes.url);
-
     let entity: Judge = {
       name: judge.attributes.name,
       position: judge.attributes.position,
@@ -22,7 +20,7 @@ const Judges = async () => {
   }
 
   return (
-    <div key="container" className="w-[85%] m-auto py-10">
+    <div key="container" className="w-full m-auto py-10 bg-dark-purple">
       <div key="judgeContainer" className="flex items-center flex-col my-20">
         <h1
           className={`bg-gradient-to-r from-primary to-secondary text-4xl bg-clip-text text-transparent font-[800] mb-6 ${cabinetBold.className}`}
@@ -31,7 +29,7 @@ const Judges = async () => {
         </h1>
         <div
           key="JudgeCards"
-          className="flex flex-wrap lg:w-full xl:w-[80%] mt-2 gap-16 justify-center"
+          className="flex flex-wrap lg:w-[80%] xl:w-[80%] mt-2 gap-16 justify-center"
         >
           {judges_real.map((judge: Judge) => (
             <JudgeCard
