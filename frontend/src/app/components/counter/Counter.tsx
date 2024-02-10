44b89075579d0375978b484da129b60f9fc280ac
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-02-10 22:04:57 +0545

    counter: hero section counter

--- frontend/src/app/components/counter/Counter.tsx
+++ frontend/src/app/components/counter/Counter.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import useCountdown from "./useCountdown";
+'use client'
+import { useEffect, useState } from "react";
 import { cabinetRegular } from "@/app/utils/fonts";
 
-const Counter: React.FC = () => {
-    const targetDate = new Date("2024-04-26T09:00:00");
-    const timeLeft = useCountdown(targetDate);
 
-    const addLeadingZero = (number: number) => {
+const Counter = () => {
+    const calculateTimeLeft = (): { days: number, hours: number, minutes: number, seconds: number } => {
+        const countDownDate = new Date("Apr 26, 2024 00:00:00").getTime();
+        const now = new Date().getTime();
+        const timeLeft = countDownDate - now;
+        return {
+            days: Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes: Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds: Math.floor((timeLeft % (1000 * 60)) / 1000)
+        };
+    };
+    
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    const addLeadingZero = (number: number): string => {
         return number < 10 ? `0${number}` : number.toString();
     };
 
     return (
-        <div className="text-magnolia w-75 h-[65] font-normal ">
+        <div className="text-magnolia w-75 h-[65] font-normal">
             <div className="flex items-center text-lg">
                 <div className="flex flex-col items-center">
                     <p
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
 import Hero from "./sections/home/Hero";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
+    <>
       <Hero/>
+      <Footer/>
+      </>
   );
 }
