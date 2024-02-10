656c51a494d5f2a85722b09c901c6bc7ee2c9fa1
CEPHAL0 <xaradxarma@gmail.com>
2024-02-10 23:32:05 +0545

    Rename page.tsx to Prize.tsx and Add in main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 
 import { cabinetBold, satoshiRegular } from "@/fonts";
 import JudgesSection from "./sections/Judges/Judges";
+import PrizesPage from "./pages/Prizes";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         This is satoshiiiii
       </p>
       <JudgesSection />
+      <PrizesPage />
       {/* <FooterSection /> */}
     </main>
   );
