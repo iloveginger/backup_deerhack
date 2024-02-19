53e03ff724b2e7570b3a8b6ed52a76dd28823507
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-02-20 00:36:35 +0545

    add: production mode

--- frontend/src/app/utils/config.ts
+++ frontend/src/app/utils/config.ts
@@ -1,7 +1,20 @@
 
+const mode = process.env.PRODUCTION ? "prod" : "dev"
+
+console.info(`Currently Running in ${mode} Mode!`)
+
 const BACKEND_URL = "http://localhost:1337"
 
-const SERVER_URL  = "http://54.164.230.35/"
+let SERVER_URL : string 
+
+if (mode == "dev"){
+    SERVER_URL = "http://localhost:1337"
+}
+else{
+    SERVER_URL  = "http://54.164.230.35" 
+}
+
+console.info(`Backend URL: ${BACKEND_URL} | Server URL: ${SERVER_URL}`)
 
 export {
     BACKEND_URL,
