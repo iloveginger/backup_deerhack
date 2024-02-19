1082817bfabb9b7b0dbae1f6a44ba9af9f2e374f
ghostuf <srijan102dahal@gmail.com>
2024-02-19 23:51:19 +0545

    Add: Image Carousel Page

---
+++ frontend/src/app/pages/ImageCarouselPage.tsx
@@ -0,0 +1,19 @@
+import ImageCarouselSection from "../sections/ImageCarousel/ImageCarouselCard";
+import long_leaf from "@/app/assets/images/long_leaf.svg";
+import Image from "next/image";
+
+export default function ImageCarouselPage() {
+  return (
+    <div className=" py-40 relative">
+        <div>
+        <ImageCarouselSection />
+        </div>
+      
+        <Image
+          src={long_leaf}
+          alt="leaf"
+          className=" absolute bottom-0 left-0 -z-10 rotate-{30} hidden lg:block"
+        />
+    </div>
+  );
+}
