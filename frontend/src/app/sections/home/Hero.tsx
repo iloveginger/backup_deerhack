6b2c7ed68cd19bf266fa8d6da489f63fa9ad2550
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-02-21 21:39:33 +0545

    fix the navbar issues and the background issues of the hero section

--- frontend/src/app/sections/home/Hero.tsx
+++ frontend/src/app/sections/home/Hero.tsx
@@ -5,12 +5,12 @@ import { cabinetBold } from "@/fonts";
 const Hero = () => {
   return (
     <div className="relative flex justify-center items-center  ">
-      <HeroIcon className="w-fit h-fit mt-5 md:mt-10" />
+      <HeroIcon className="w-screen h-fit mt-5 md:mt-10" />
       <div className="absolute gap-[25px] top-[80px] flex flex-col text-center lg:top-64 ">
         <p className={`text-3xl font-extrabold text-secondary ${cabinetBold.className}`}>
           26th - 29th April
         </p>
-        <MainLogo className="w-fit h-fit sm:w-1 sm:h-3 " />
+        <MainLogo className="w-fit h-fit " />
       </div>
     </div>
   );
