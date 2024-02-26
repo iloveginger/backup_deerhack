5cd43703c8fbdab933509e0eac731cf872d13e02
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-02-26 21:29:59 +0545

    fixed new hero section for mobile

--- frontend/src/app/sections/home/Hero.tsx
+++ frontend/src/app/sections/home/Hero.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import MainLogo from "@/app/assets/icons/MainLogo";
 import HeroIcon from "@/app/assets/icons/Hero";
+import HomeNewHero from "@/app/assets/icons/HomeNewHero";
 import { cabinetBold } from "@/fonts";
 
 const Hero = () => {
@@ -10,18 +11,14 @@ const Hero = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 580) {
-        setLogoSize({ width: 150, height: 50 });
-      }
-
-      else if (window.innerWidth < 768) {
+      if (window.innerWidth < 768) {
         setLogoSize({ width: 200, height: 87 });
-        setTextSize('text-lg'); 
+        setTextSize('text-lg');
       }
 
       else {
         setLogoSize({ width: 345, height: 150 });
-        setTextSize('text-3xl'); 
+        setTextSize('text-3xl');
       }
     };
 
@@ -35,7 +32,11 @@ const Hero = () => {
 
   return (
     <div className="relative flex justify-center ">
-      <HeroIcon className="w-screen h-fit mt-5 " />
+      {
+        window.innerWidth < 576 ?
+          <HomeNewHero className=" object-fit w-screen h-fit " /> :
+          <HeroIcon className="w-screen h-fit mt-5 " />
+      }
       <div className="absolute gap-[30px] top-[90px] flex flex-col text-center lg:top-48">
         <p className={`${textSize} font-bold text-secondary ${cabinetBold.className}`}>
           5th - 7th May
