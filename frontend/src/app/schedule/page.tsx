7bae43b879d890064ff161349a4d3dd6aa59ae05
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-02-27 00:52:41 +0545

    Add Announced component to resources, schedule, and winners pages

--- frontend/src/app/schedule/page.tsx
+++ frontend/src/app/schedule/page.tsx
@@ -1,19 +1,13 @@
-import HeroIcon from "@/app/assets/icons/Hero";
-import { cabinetExtraBold } from "@/fonts";
-import GoBackToHome from "../components/GoBack/GoBackToHome";
+import Announced from "../components/Announced/Announced";
 
-const Hero = () => {
+const schedule = () => {
     return (
-        <div className=" flex justify-center ">
-            <HeroIcon className="w-fit fixed left-0" />
-            <div className="absolute gap-[40px] top-[190px] flex flex-col text-center lg:top-48 sm:top-56">
-                <p className={`${cabinetExtraBold.className} text-secondary-gradient-color-reverse w-fit text-5xl px-4 text-center`}>
-                    Schedule Will Be Announced Soon!
-                </p>
-                <GoBackToHome />
-            </div>
-        </div>
+       <>
+       <Announced
+              description="Schedule Will Be Announced Soon!"
+            />
+       </>
     );
 };
 
-export default Hero;
+export default schedule;
---
+++ frontend/src/app/winners/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Announced from '../components/Announced/Announced'
+
+const winners = () => {
+    return (
+        <>
+            <Announced
+                description="Winners Will Be Announced Soon!"
+            />
+        </>
+    )
+}
+
+export default winners
