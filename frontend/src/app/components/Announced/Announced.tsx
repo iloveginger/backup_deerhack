0bf917be2555a7ffa918cd4083556c54b23e7fdb
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-02-27 21:56:16 +0545

    svg to backgroundImage

--- frontend/src/app/components/Announced/Announced.tsx
+++ frontend/src/app/components/Announced/Announced.tsx
@@ -1,11 +1,11 @@
-import HeroIcon from "@/app/assets/icons/Hero";
 import { cabinetExtraBold } from "@/fonts";
 import GoBackToHome from "../GoBack/GoBackToHome";
+import styles from "./Announced.module.css";
 
 const Announced = ({ description }: { description: string }) => {
   return (
-    <div className=" flex justify-center ">
-      <HeroIcon className="w-fit fixed left-0" />
+    <div className={` ${styles.announced_background_container}  flex justify-center `}>
+      
       <div className="absolute gap-[40px] top-[190px] flex flex-col text-center lg:top-48 sm:top-56">
         <p
           className={`${cabinetExtraBold.className} text-secondary-gradient-color-reverse w-fit text-5xl px-4 text-center`}
