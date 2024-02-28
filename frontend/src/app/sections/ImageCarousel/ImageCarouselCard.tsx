9469c47fae57bcdbe0eacf39af571e84a8d663ce
ghostuf <srijan102dahal@gmail.com>
2024-02-28 23:02:33 +0545

    Resolved: Issue #155 resolved, Svg Warnings Resolved

--- frontend/src/app/sections/ImageCarousel/ImageCarouselCard.tsx
+++ frontend/src/app/sections/ImageCarousel/ImageCarouselCard.tsx
@@ -1,4 +1,5 @@
 import { data } from "@/app/components/ImageCarousel/mockData";
+import { dataTwo } from "@/app/components/ImageCarousel/mockDataTwo";
 import ImageCard from "@/app/components/ImageCarousel/imageCard";
 
 export default function ImageCarouselSection() {
@@ -20,14 +21,14 @@ export default function ImageCarouselSection() {
 
       <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)">
         <ul className="flex items-center justify-center md:justify-start [&_li]:mx-2 [&_img]:max-w-none animate-inf-scroll">
-          {data.map((e, i) => (
-            <ImageCard key={i} image={`${e.img}`} />
+          {dataTwo.map((x, i) => (
+            <ImageCard key={i} image={`${x.img}`} />
           ))}
         </ul>
 
         <ul className="flex items-center justify-center md:justify-start [&_li]:mx-2 [&_img]:max-w-none animate-inf-scroll">
-          {data.map((e, i) => (
-            <ImageCard key={i} image={`${e.img}`} />
+          {dataTwo.map((x, i) => (
+            <ImageCard key={i} image={`${x.img}`} />
           ))}
         </ul>
       </div>
