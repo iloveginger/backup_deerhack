04c141e888cf197edc8244148b3f8e70577b1744
ashshelby <nepalidude3@gmail.com>
2024-03-05 00:19:40 +0545

    add: actual sponsors

--- frontend/src/app/components/Sponsors/Sponsor.tsx
+++ frontend/src/app/components/Sponsors/Sponsor.tsx
@@ -1,10 +1,10 @@
-import { ReactElement } from "react";
-export default function Sponsor({ svg }: { svg: ReactElement }) {
+import Image, { StaticImageData } from "next/image";
+export default function Sponsor({ image }: { image: StaticImageData }) {
   return (
     <div className="flex flex-row flex-wrap justify-center items-center w-[15rem]">
       <div className=" inset-0 flex flex-row justify-center items-center">
         <div className=" mb-[2rem] mx-[1rem] w-[12rem] h-[12rem] rounded-full bg-[#241A4A] flex flex-row justify-center items-center">
-          {svg}
+          <Image src={image} width={100} height={100} alt="Sponsor Logo" />
         </div>
       </div>
     </div>
