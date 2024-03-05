78f94aeb69e008921c87396159743c65173feb7b
chrisdvkta <chrisdevkota77@gmail.com>
2024-03-05 23:11:13 +0545

    fixed sponsors section border

--- frontend/src/app/components/Sponsors/Sponsor.tsx
+++ frontend/src/app/components/Sponsors/Sponsor.tsx
@@ -2,11 +2,12 @@ import Image, { StaticImageData } from "next/image";
 export default function Sponsor({ image }: { image: StaticImageData }) {
   return (
     <div className="flex flex-row flex-wrap justify-center items-center w-[15rem]">
-      <div className=" inset-0 flex flex-row justify-center items-center">
-        <div className=" mb-[2rem] mx-[1rem] w-[12rem] h-[12rem] rounded-full bg-[#241A4A] flex flex-row justify-center items-center">
-          <Image src={image} width={100} height={100} alt="Sponsor Logo" />
+      <div className="inset-0 flex flex-row justify-center items-center">
+        <div className=" w-[5.2rem]  rounded-full flex flex-row justify-center items-center">
+          <Image src={image} width={100} height = {100} alt="Sponsor Logo" className="object-cover w-full h-full" />
         </div>
       </div>
     </div>
   );
 }
+
