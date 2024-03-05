5aeb71139633edf92f2821d668420ce3cbed058b
chrisdvkta <chrisdevkota77@gmail.com>
2024-03-05 23:54:26 +0545

    Modify:Sponsors Data Fetch

--- frontend/src/app/components/Sponsors/Sponsor.tsx
+++ frontend/src/app/components/Sponsors/Sponsor.tsx
@@ -1,10 +1,13 @@
 import Image, { StaticImageData } from "next/image";
-export default function Sponsor({ image }: { image: StaticImageData }) {
+import Link from "next/link";
+export default function Sponsor({ image,alt,url }: { image: StaticImageData,alt:string,url:string }) {
   return (
     <div className="flex flex-row flex-wrap justify-center items-center w-[15rem]">
       <div className="inset-0 flex flex-row justify-center items-center">
-        <div className=" w-[5.2rem]  rounded-full flex flex-row justify-center items-center">
-          <Image src={image} width={100} height = {100} alt="Sponsor Logo" className="object-cover w-full h-full" />
+        <div className="rounded-full flex flex-row justify-center items-center">
+          <Link href = {url} target="_blank">
+            <Image src={image} alt = {alt} width={100} height = {100} loading="lazy" className="object-cover w-full h-full" />
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/app/sections/Sponsors/SponsorsSection.tsx
+++ frontend/src/app/sections/Sponsors/SponsorsSection.tsx
@@ -2,67 +2,35 @@ import Sponsor from "@/app/components/Sponsors/Sponsor";
 import NetworkSVG from "@/app/assets/images/network";
 import { cabinetMedium, cabinetExtraBold } from "@/app/utils/fonts";
 import TitleSponsor from "@/app/components/Sponsors/TitleSponsor";
-import DeerholdLogo from "@/app/assets/images/sponsors/deerhold.png";
-import MountStradaLogo from "@/app/assets/images/sponsors/mount_strada.png";
-import SegwayLogo from "@/app/assets/images/sponsors/segway.png";
+import { sponsors } from "@/app/components/Sponsors/data";
 
 export default function SponsorsSection() {
   return (
     <div>
+
+
       <div className="mb-[5rem] flex flex-col justify-center items-center">
         <div className={`mb-8 ${cabinetExtraBold.className} headings`}>
           Our Sponsors
         </div>
         <TitleSponsor image={<NetworkSVG height={100} width={100} />} />
       </div>
-      {/* <div className="flex flex-col justify-center items-center mb-[5rem]">
-        <div
-          className={`${cabinetMedium.className} m-10 text-white text-3xl font-extrabold tracking-normal`}
-        >
-          Platinum Sponsors
-        </div>
-
-        <Sponsor image={<PuzzleSVG height={100} width={100}/>} />
-      </div> */}
+      
+    {sponsors.map((sponsorType)=>(
       <div className="flex flex-col justify-center items-center mb-[5rem]">
-        {/* <div
-          className={`${cabinetMedium.className} m-10 text-white text-3xl font-extrabold tracking-normal`}
-        >
-          Gold Sponsors
-        </div>
-        <div className="mx-[15rem]  flex flex-row flex-wrap justify-center items-center">
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-          <Sponsor svg={<PuzzleSVG height={100} width={100} />} />
-        </div> */}
+        
         <div
           className={`${cabinetMedium.className} m-10 text-white text-3xl font-extrabold tracking-normal`}
         >
-          Silver Sponsors
+          {sponsorType.name}
         </div>
-        <div className="mx-[15rem]  flex flex-row flex-wrap justify-center items-center">
-          <Sponsor image={DeerholdLogo} />
-        </div>
-        <div
-          className={`${cabinetMedium.className} m-10 text-white text-3xl font-extrabold tracking-normal`}
-        >
-          Coffee Partner
-        </div>
-        <div className="mx-[15rem]  flex flex-row flex-wrap justify-center items-center">
-          <Sponsor image={MountStradaLogo} />
-        </div>
-        <div
-          className={`${cabinetMedium.className} m-10 text-white text-3xl font-extrabold tracking-normal`}
-        >
-          Mobility Partner
-        </div>
-        <div className="mx-[15rem]  flex flex-row flex-wrap justify-center items-center">
-          <Sponsor image={SegwayLogo} />
-        </div>
-      </div>
+        {sponsorType.sponsors.map((sponsor)=>(
+            <div className="mx-[15rem]  flex flex-row flex-wrap justify-center items-center">
+          <Sponsor image={sponsor.image} alt={sponsor.name} url = {sponsor.url}/>
+            </div>
+     ))}     
+    </div>
+     ))}
     </div>
   );
 }
