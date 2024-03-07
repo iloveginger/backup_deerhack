2ccc11155d5da34d6d15f2bf359694de76e51402
CEPHAL0 <xaradxarma@gmail.com>
2024-03-08 00:31:24 +0545

    Changed Cards

--- frontend/src/app/components/Polygon/Polygon.tsx
+++ frontend/src/app/components/Polygon/Polygon.tsx
@@ -9,46 +9,46 @@ import PolygonSVG from "@/app/assets/images/polygon";
 
 export default function Polygon(): ReactElement {
   return (
-    <div className=" bg-violet w-64 md:w-48 lg:w-72 rounded-xl flex items-center py-3 md:py-0 px-2 md:px-0">
-      <div className="flex flex-col px-4 lg:px-6 py-5 gap-3 lg:gap-5">
+    <div className=" bg-violet w-full sm:w-64 md:w-48 lg:w-72 rounded-xl sm:items-center py-3 md:py-0 px-2 md:px-0 h-[22rem] sm:h-auto ">
+      <div className="flex flex-col px-4 lg:px-6 gap-3 lg:gap-5 h-full justify-around py-4">
         <div className="flex justify-start gap-2 items-center">
           <PolygonSVG height={50} width={50} />
           <p
-            className={`text-primary-gradient-color ${cabinetExtraBold.className} text-xl lg:text-3xl`}
+            className={`text-primary-gradient-color ${cabinetExtraBold.className}  text-2xl sm:text-xl lg:text-3xl`}
           >
             Polygon
           </p>
         </div>
         <div className="flex flex-col">
           <p
-            className={`${cabinetBold.className} text-white text-sm lg:text-xl`}
+            className={`${cabinetBold.className} text-white text-xl sm:text-sm lg:text-xl`}
           >
             Ethereum Track
           </p>
           <p
-            className={`${cabinetLight.className} text-[0.6rem] text-white lg:text-sm`}
+            className={`${cabinetLight.className} text-[0.7rem] sm:text-[0.6rem] text-white lg:text-sm`}
           >
             best hack built on Ethereum
           </p>
           <p
-            className={`${cabinetBold.className} text-secondary text-xs lg:text-base`}
+            className={`${cabinetBold.className} text-secondary text-base sm:text-xs lg:text-base`}
           >
             worth $150
           </p>
         </div>
         <div className="flex flex-col">
           <p
-            className={`${cabinetBold.className} text-white text-sm lg:text-xl`}
+            className={`${cabinetBold.className} text-white text-xl sm:text-sm lg:text-xl`}
           >
             Ethereum + Polygon <br /> Track
           </p>
           <p
-            className={`${cabinetLight.className} text-[0.6rem] text-white lg:text-sm`}
+            className={`${cabinetLight.className} text-[0.7rem] sm:text-[0.6rem] text-white lg:text-sm`}
           >
             best hack built on Ethereum + Polygon
           </p>
           <p
-            className={`${cabinetBold.className} text-secondary text-xs lg:text-base`}
+            className={`${cabinetBold.className} text-secondary text-base sm:text-xs lg:text-base`}
           >
             worth $150
           </p>
