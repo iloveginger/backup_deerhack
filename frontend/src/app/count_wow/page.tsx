545bee2b4ee61ebf018f3b7dadb9cbfb4ff12316
ashshelby <nepalidude3@gmail.com>
2024-03-24 23:10:29 +0545

    canada time -> nepal time

--- frontend/src/app/count_wow/page.tsx
+++ frontend/src/app/count_wow/page.tsx
@@ -10,10 +10,11 @@ export default async function Page(){
     }
     const data = await response.json()
     const participant_count = data.participants_count;
+    const nepal_time = new Date().toLocaleTimeString('en-US',{timeZone:"Asia/Kathmandu"})
 
     return (
         <div className={`flex justify-center mt-[20%] headings color-white ${cabinetBlack}`}>
-            <h1>Participant Count as of {new Date().toLocaleTimeString()} is <div className="font-black">{participant_count}</div> </h1>
+            <h1>Participant Count as of {nepal_time} is <div className="font-black">{participant_count}</div> </h1>
         </div>
     )
 }
