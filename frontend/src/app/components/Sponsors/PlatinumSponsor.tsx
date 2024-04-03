f4a1b7b479eac729fd311bd07ac2c47e480d01e3
chrisdvkta <chrisdevkota77@gmail.com>
2024-04-03 22:27:36 +0545

    Add:Sponsors

--- frontend/src/app/components/Sponsors/PlatinumSponsor.tsx
+++ frontend/src/app/components/Sponsors/PlatinumSponsor.tsx
@@ -14,7 +14,7 @@ const PlatinumSponsor = () => {
       </div>
       <div className=" flex flex-wrap md:flex-row flex-col gap-20 justify-center items-center ">
         <div className=" flex flex-row  flex-wrap justify-center items-center gap-20">
-          <div key="platinumSponsor" className="flex flex-row  justify-center items-center w-[11rem] md:w-[20rem] h-[11rem]  rounded-full mb-10">
+          <div key="platinumSponsor" className="flex flex-row  justify-center items-center w-[13rem] md:w-[20rem] h-[11rem]  rounded-full mb-10">
             <Link href="https://www.securitypalhq.com" target="_blank">
               <Image
                 src={securityPal}
